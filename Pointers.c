ed1bed3e26dc4c17d7ae078cc2afa943dfcee7e2
ABHIJEET KESARWANI <74383254+AbhijeetK1N6@users.noreply.github.com>
2023-08-31 11:48:39 +0530

    Create Pointers.c

---
+++ Pointers.c
@@ -0,0 +1,16 @@
+//Printing Pointer Values and their Addresses
+/*
+#include<stdio.h>
+int main()
+{
+    int val=67,i=0;
+    int *ptr;
+    int maty[2][2]={{9.1,6.2},{2.6,1.9}};
+    ptr=&maty[0][0];
+    
+    for(i=0;i<4;i++){
+        printf("Val = %d address = %lu\n",*ptr,ptr);
+        ptr++;
+    }
+}
+*/
