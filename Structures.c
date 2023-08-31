1ee49c2233795ca05ecdeb3f876ec58be79c5691
ABHIJEET KESARWANI <74383254+AbhijeetK1N6@users.noreply.github.com>
2023-08-31 17:34:54 +0530

    Update Structures.c

--- Structures.c
+++ Structures.c
@@ -45,3 +45,29 @@ int main()
     printf("stu name %s\n",stu_obj.name);
     printf("stu salary %f\n",stu_obj.salary);
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//CODE - 3
+
+
+#include<stdio.h>
+#include <string.h>
+typedef struct{
+    char *name;
+    int val;
+}Node;
+
+int main()
+{
+Node n1,n2,n3;
+char name[]="Pune";
+n1=(Node){name,1337};
+n2=n1;
+
+n3.val=n1.val;
+n3.name=strdup(n1.name);
+
+printf("n2.name=%s and &n2.name=%p\n",n2.name,n2.name);
+  printf("n1.name=%s and &n1.name=%p\n",n1.name,n1.name);
+   printf("n3.name=%s and &n3.name=%p\n",n3.name,n3.name);
+}
